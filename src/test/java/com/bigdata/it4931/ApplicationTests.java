9dd168aeb0c0268224e9c0f9892f38818a79fdc5
Hai Pham Ngoc <ngochai285nd@gmail.com>
2024-10-09 07:40:41 +0700

    init project

---
+++ src/test/java/com/bigdata/it4931/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.it4931;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
